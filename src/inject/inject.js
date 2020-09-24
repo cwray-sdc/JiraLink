72d29fb093d2c62f5ac3e7015a1c8fec82de533d
Charlie <cwray@stamps.com>
2020-09-24 13:11:09 -0700

    Initial commit

---
+++ src/inject/inject.js
@@ -0,0 +1,35 @@
+chrome.extension.sendMessage({}, (response) => {
+	const readyStateCheckInterval = setInterval(() => {
+	if (document.readyState === "complete") {
+		clearInterval(readyStateCheckInterval);
+
+		const body = document.getElementsByTagName('body')[0];
+
+		const targets = ['js-issue-title'];
+
+		
+		targets.map(target => {
+
+			const links = body.getElementsByClassName(target);
+
+			for (let index = 0; index < links.length; index++) {
+				const link = links[index];
+	
+				let c;
+
+				if (link.innerText.indexOf(':') !== -1) {
+					c = link.innerText.split(':');
+				} else if (link.innerText.indexOf(':') !== -1) {
+					c = link.innerText.split('-');
+				}
+	
+				if (c) {
+					link.innerHTML = `<a target="_blank" href="http://jira/browse/${c[0].trim()}">${c[0]}</a>:${c[1]}`;
+				}
+			}
+		})
+		
+
+	}
+	}, 10);
+});
