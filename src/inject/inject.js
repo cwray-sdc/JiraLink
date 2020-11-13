9b5818cb7c409e71562a51bcc3d5c4ba24bc04a2
Charles Wray <51002931+cwray-sdc@users.noreply.github.com>
2020-11-13 13:46:13 -0800

    Update styles. Rename symbol for readability. Added a couple comments

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -5,7 +5,8 @@ chrome.extension.sendMessage({}, (response) => {
 
 			const body = document.getElementsByTagName('body')[0];
 
-			if (window.location.host.indexOf('atlassian') === -1) {
+			// If you're on github
+			if (window.location.host.indexOf('github') !== -1) {
 				const targets = ['js-issue-title'];
 
 				targets.map(target => {
@@ -15,29 +16,31 @@ chrome.extension.sendMessage({}, (response) => {
 					for (let index = 0; index < links.length; index++) {
 						const link = links[index];
 			
-						let c;
+						let jiraIssueTitle;
 		
 						if (link.innerText.indexOf(':') !== -1) {
-							c = link.innerText.split(':');
+							jiraIssueTitle = link.innerText.split(':');
 						} else if (link.innerText.indexOf(' - ') !== -1) {
-							c = link.innerText.split(' - ');
+							jiraIssueTitle = link.innerText.split(' - ');
 						}
 			
-						if (c && c.length === 2 && c[0].toLowerCase().indexOf('webapps') === 0) {
+						if (jiraIssueTitle && jiraIssueTitle.length === 2 && jiraIssueTitle[0].toLowerCase().indexOf('webapps') === 0) {
 							link.innerHTML = `
 								<a 
 									target="_blank" 
-									href="http://jira/browse/${c[0].trim()}/?githubUrl=${window.location.href}&jiraId=${c[0].trim()}"
-								>${c[0]}</a>: ${c[1].trim()}
+									href="http://jira/browse/${jiraIssueTitle[0].trim()}/?githubUrl=${window.location.href}&jiraId=${jiraIssueTitle[0].trim()}"
+								>${jiraIssueTitle[0]}</a>: ${jiraIssueTitle[1].trim()}
 								<a 
 									target="_blank" 
-									style="background-color: #0366d6; color: #fff; padding: 2px; border-radius: 3px; font-size: 1rem;" 
-									href="http://jira/browse/${c[0].trim()}/?githubUrl=${window.location.href}&jiraId=${c[0].trim()}">Link PR to Jira</a>
+									style="background-color: #0366d6; color: #fff; padding: 2px; border-radius: 3px; font-size: 1rem; display: inline-block;" 
+									href="http://jira/browse/${jiraIssueTitle[0].trim()}/?githubUrl=${window.location.href}&jiraId=${jiraIssueTitle[0].trim()}">Link PR to Jira</a>
 							`;
 						}
 					}
 				})
-			} else {
+			} 
+			// If you're on Jira
+			else if (window.location.host.indexOf('atlassian') !== -1) {
 				const urlParams = new URLSearchParams(window.location.search);
 
 				if (urlParams.get('githubUrl')) {
